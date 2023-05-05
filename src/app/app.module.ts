eeaaec2a32f6fb535a84d559db7626900d536715
charles Kakai <kaksunchained@gmail.com>
2023-05-05 08:48:29 +0300

    Changed the form to use reactive forms in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -18,6 +18,7 @@ import { MediaFormComponent } from './media-form/media-form.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     NgbModule
   ],
   providers: [],
--- src/app/media-form/media-form.component.ts
+++ src/app/media-form/media-form.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-media-form',
   templateUrl: './media-form.component.html',
   styleUrls: ['./media-form.component.css']
 })
-export class MediaFormComponent {
+export class MediaFormComponent implements OnInit {
+  myForm: FormGroup;
+ 
+  ngOnInit(): void {
+      this.myForm = new FormGroup({
+        name: new FormControl(''),
+        medium: new FormControl('Movies'),
+        rating: new FormControl(''),
+        description: new FormControl('')
+      })
+  }
   onSubmit(mediaItem){
     console.log(mediaItem)
   }
