6fa77cf553fcb0df96c694ff3803c68ff31d90c9
charles Kakai <kaksunchained@gmail.com>
2023-05-05 08:21:40 +0300

    Added a form to the app using template driven forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaListComponent } from './media-list/media-list.component';
+import { MediaFormComponent } from './media-form/media-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MediaItemComponent,
-    MediaListComponent
+    MediaListComponent,
+    MediaFormComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule
   ],
   providers: [],
