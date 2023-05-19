fb1afb9a3e7910e05e115aaccfb09f7270480a11
charles Kakai <kaksunchained@gmail.com>
2023-05-19 22:36:25 +0300

    Started implementing route guards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SecurityService } from './shared/security/security.service';
+import { AppUser } from './security/app-user';
+import { AppUserAuth } from './security/app-user-auth';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learning-app'; 
+  securityObject: AppUserAuth = new AppUserAuth();
+  constructor(private securityService:SecurityService){
+    this.securityObject = securityService.securityObject;
+  }
+  logout():void{
+    this.securityService.logout();
+    this.securityObject = this.securityService.securityObject;
+  }
 }
